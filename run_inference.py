82357e7782398a861805bd234c670d9b305323a0
realisticattorney <g.axl.aquila@gmail.com>
2024-02-27 06:08:28 -0300

    Working on a new file

--- run_inference.py
+++ run_inference.py
@@ -4,7 +4,7 @@ from ultralytics import (
     YOLO,
 )  # this is the ultralytics library, which contains the YOLO model,a neural network that can detect objects in images
 
-model = YOLO("yolov8x.pt")
+model = YOLO("yolov5xu.pt")
 box_annotator = sv.BoundingBoxAnnotator()
 
 
@@ -15,15 +15,12 @@ def callback(frame: np.ndarray, _: int) -> np.ndarray:
 
 
 sv.process_video(
-    source_path="inputvids/resumen_fulbo.mp4",
+    source_path="inputvids/IMG_0667.mp4",
     target_path="outputvids/resultado.mp4",
     callback=callback,
 )
 
 
-
-#GOALS:
+# GOALS:
 # 1) Detect players and ball
 # 2) Detect the field lines and measure distances so we can calculate the size of the field + the size of the players + the speed of the players and the ball
-
-    
